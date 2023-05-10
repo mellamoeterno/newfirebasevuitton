e2746641c0f6017fd99079c274cc9243a538d8ef
Samuel Enoque <thesonlike@gmail.com>
2023-05-10 16:39:42 -0300

    comments

--- viteReact/vite-project/src/App.jsx
+++ viteReact/vite-project/src/App.jsx
@@ -15,7 +15,9 @@ import './App.css'
 //and is usually mounted onto the DOM using ReactDOM.render() in the main.jsx file.
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) 
+  
+//hook called useState to declare a state variable count and a function setCount to update the value of the count variable. The initial value of count is set to 0.
 
   return (
     <>
@@ -32,6 +34,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {/* the button element calls the setCount function when it is clicked, updating the value of count. The value of count is then displayed in the button text. */}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -42,5 +45,12 @@ function App() {
     </>
   )
 }
+
+
+
+
 //jsx component that can be only used in .jsx files ^^^^
+
 export default App
+
+// The component ends with an export statement, exporting the App component as the default export of the file, which can be imported and used in other parts of the project.
--- viteReact/vite-project/src/main.jsx
+++ viteReact/vite-project/src/main.jsx
@@ -3,9 +3,47 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//main.jsx is the main entry point of a React application,like its name 'main', where the root component 
+//of the application is mounted onto the DOM.
+//
+//the root component is wrapped in a ReactDOM.render() function 
+//call and passed to a DOM element where the application will be rendered. main.jsx is where this function call is made.
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(//.render from app.jsx
+
+//The createRoot() method allows for asynchronous rendering and improves performance. 
+//It returns a Root object that represents the root of a React tree.
+//
+//This code is equivalent to using ReactDOM.render() 
+//to mount the App component onto the DOM, but using createRoot() instead allows for improved performance and better handling of asynchronous updates.
+//
+//createRoot() method is called with the argument document.getElementById('root'), 
+//which retrieves the DOM element with the ID of 'root'. This element will serve as the root node of the React tree.
+//
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+// This is the ReactDOM method createRoot() being used to create a root element for a React application, 
+//and then rendering the App component inside it using render().
 
+
+// -
+// -
+// -
+// -
+// -
+// -
+
+
+//about REACT.STRICT MODE ^^^^^^^^
+//
+//When <React.StrictMode> is used to wrap the top-level component(s) 
+//(in this case, App), React will perform additional checks and emit warnings 
+//in the console for a number of potential problems. These warnings can help you catch and fix issues such as:
+
+// Deprecated lifecycle methods or APIs
+// Unsafe legacy lifecycle methods
+// Unsafe state updates in components
+// Violations of the recommended data flow practices
